99bb494bc943133d648d47909b385d99879c8808
Yvan BARTHÉLEMY <ybarthelemy@free.fr>
2011-03-27 14:42:59 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JTopStats::Application.config.secret_token = 'cbb86f452aaa928d63b91c4cebae9bcb9b8c9067b07847df9527e1970dd17441ec28c76b50bbe720afd93927dbc3433bbf7fca98b0de4076577ad864f5f539df'
