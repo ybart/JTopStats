201754c2b60a531ff5e01196d366af38d815903f
Yvan BARTHÉLEMY <ybarthelemy@free.fr>
2011-06-13 02:38:38 +0200

    Translations

--- app/helpers/jtops_helper.rb
+++ app/helpers/jtops_helper.rb
@@ -1,9 +1,9 @@
 module JtopsHelper
   def quick_nav
     nav = ""
-    nav += link_to 'Précédent', jtop_path(@jtop.id - 1) if @jtop.id > 2
-    nav += ' | ' + link_to('Suivant', jtop_path(@jtop.id + 1)) if @jtop.id < Jtop.aggregate(:id.max)
-    nav += " | Aller au J-Top:" + form_tag(jtops_path, :method => :get, :class => 'inline') do
+    nav += link_to I18n.t('previous'), jtop_path(@jtop.id - 1) if @jtop.id > 2
+    nav += ' | ' + link_to(I18n.t('next'), jtop_path(@jtop.id + 1)) if @jtop.id < Jtop.aggregate(:id.max)
+    nav += ' | ' + I18n.t('jtop.go') + form_tag(jtops_path, :method => :get, :class => 'inline') do
       text_field_tag(:id, nil, :size => 3) + submit_tag('Go')
     end
     return nav.html_safe
